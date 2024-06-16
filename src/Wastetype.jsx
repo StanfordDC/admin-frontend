6402376a91f37cac849e98b2f8079d5764b64ac8
StanfordDC <stanforddchandra@gmail.com>
2024-06-16 23:13:20 +0800

    Removed unused import

--- src/Wastetype.jsx
+++ src/Wastetype.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useEffect } from "react";
 import { useState } from 'react'
 import WasteItems from "./WasteItems";
 function Wastetype(){
