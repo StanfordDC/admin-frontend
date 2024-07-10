930292883e0808f718f2bd27c6007948432cf0cf
StanfordDC <stanforddchandra@gmail.com>
2024-07-10 22:59:04 +0800

    Added header and sidebar for update page

--- src/pages/UpdateWasteType.jsx
+++ src/pages/UpdateWasteType.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useState } from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
 function UpdateWasteType({prop}){
+    const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+    const OpenSidebar = () => {
+      setOpenSidebarToggle(!openSidebarToggle)
+    }
     const[item, setItem] = useState(prop.item);
     const[material, setMaterial] = useState(prop.material);
     const[link, setLink] = useState(prop.links == null ? '' : prop.links[0]);
@@ -48,37 +54,41 @@ function UpdateWasteType({prop}){
 
     return(
         <form action="" onSubmit={handleSubmit}>
-             <main className='main-container'>
-                <div className='main-title'>
-                    <h2>UPDATE WASTE TYPE</h2>
+             <main className='grid-container'>
+                <Header OpenSidebar={OpenSidebar}/>
+                <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+                <div className="main-container">
+                    <div className='main-title'>
+                        <h2>UPDATE WASTE TYPE</h2>
+                    </div>
+                    <div className='main-title'>
+                        <h3>ITEM NAME</h3>
+                    </div>
+                    <input type="text" value={item} onChange={handleItemChange}/>
+                    <div className='main-title'>
+                        <h3>RECYCLING INSTRUCTIONS</h3>
+                    </div>
+                    <textarea id="text-box" value={instructions} onChange={handleInstructionsChange}></textarea>
+                    <div className='main-title'>
+                        <h3>LINK FOR FURTHER INFORMATION</h3>
+                    </div>
+                    <input type="text" value={link} onChange={handleLinkChange}/>
+                    <div className='main-title'>
+                        <h3>WASTE MATERIAL</h3>
+                    </div>
+                    <input type="text" value={material} onChange={handleMaterialChange}/>
+                    <div className='main-title'>
+                        <h3>RECYCLABLE</h3>
+                    </div>
+                    <select name="recyclable" id="" value={recyclable} onChange={handleRecyclableChange}>
+                        <option value="true" selected>Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                    <br /><br />
+                    {!isPending && <button className="add-button">UPDATE</button>}
+                    {isPending && <button className="add-button" disabled>WASTE TYPE UPDATED</button>}
                 </div>
-                <div className='main-title'>
-                    <h3>ITEM NAME</h3>
-                </div>
-                <input type="text" value={item} onChange={handleItemChange}/>
-                <div className='main-title'>
-                    <h3>RECYCLING INSTRUCTIONS</h3>
-                </div>
-                <textarea id="text-box" value={instructions} onChange={handleInstructionsChange}></textarea>
-                <div className='main-title'>
-                    <h3>LINK FOR FURTHER INFORMATION</h3>
-                </div>
-                <input type="text" value={link} onChange={handleLinkChange}/>
-                <div className='main-title'>
-                    <h3>WASTE MATERIAL</h3>
-                </div>
-                <input type="text" value={material} onChange={handleMaterialChange}/>
-                <div className='main-title'>
-                    <h3>RECYCLABLE</h3>
-                </div>
-                <select name="recyclable" id="" value={recyclable} onChange={handleRecyclableChange}>
-                    <option value="true" selected>Yes</option>
-                    <option value="false">No</option>
-                </select>
-                <br /><br />
-                {!isPending && <button className="add-button">UPDATE</button>}
-                {isPending && <button className="add-button" disabled>WASTE TYPE UPDATED</button>}
-        </main>
+            </main>
         </form>
     );
 }
