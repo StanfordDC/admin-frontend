bcd1f8e88bcb97992af71430aee56783efb341dd
StanfordDC <stanforddchandra@gmail.com>
2024-07-23 15:55:10 +0800

    Fixed api naming

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -1,6 +1,6 @@
 export const BASE_ENDPOINT = 'http://localhost:8080';
 export const METRICS_HISTORY_ENDPOINT = (startYear, startMonth, endYear, endMonth) =>
-     `${BASE_ENDPOINT}/responses/historystartYear=${startYear}&startMonth=${startMonth}&endYear=${endYear}&endMonth=${endMonth}`;
+     `${BASE_ENDPOINT}/responses/history/startYear=${startYear}&startMonth=${startMonth}&endYear=${endYear}&endMonth=${endMonth}`;
 export const METRICS_ENDPOINT = `${BASE_ENDPOINT}/responses/metrics`;
 export const RESPONSES_ENDPOINT = `${BASE_ENDPOINT}/responses`;
 export const WASTETYPE_ENDPOINT = `${BASE_ENDPOINT}/waste-type`;
